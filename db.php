3795ea5edd727337a2f60d770782085825e583af
Akram Hossain Rahat <akramhossainrahat@users.noreply.github.com>
2018-02-23 22:05:53 +0600

    Add files via upload

---
+++ db.php
@@ -0,0 +1,27 @@
+<?php
+
+
+//Database connection.
+
+$con = MySQLi_connect(
+
+    "localhost", //Server host name.
+
+    "root", //Database username.
+
+    "", //Database password.
+
+    "dropdown_live_search" //Database name or anything you would like to call it.
+
+);
+
+
+//Check connection
+
+if (MySQLi_connect_errno()) {
+
+    echo "Failed to connect to MySQL: " . MySQLi_connect_error();
+
+}
+
+?>
